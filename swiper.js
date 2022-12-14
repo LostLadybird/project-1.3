e08147bc677a54e1f2c674c5e7f5988ee579ea66
LostLadybird <julia_mg@rambler.ru>
2022-12-15 00:24:43 +0300

    1.5

--- swiper.js
+++ swiper.js
@@ -4,4 +4,18 @@ new Swiper('.swiper-container', {
   pagination: {
     el: '.swiper-pagination',
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.3,
+      spaceBetween: 16,
+    },
+    480: {
+      slidesPerView: 1.8,
+      spaceBetween: 24,
+    },
+    640: {
+      slidesPerView: 2.8,
+      spaceBetween: 40,
+    },
+  }
 });
